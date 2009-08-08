8255453c2fae5f44d53492a1e5a965781dd351ca
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2009-08-08 08:15:52 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@20 906dfecc-7149-11de-8b73-c1edde54a3c7

--- data_struct/idsl/idsl_hash.c
+++ data_struct/idsl/idsl_hash.c
@@ -28,7 +28,7 @@ typedef struct _hash_element {
 
 typedef void* (* hash_fct_ptr_t) (void *, void*, void*);
 
-struct infos {
+struct _infos {
 	hash_fct_ptr_t	f;
 	void*			ud;
 	void*			d;
@@ -213,5 +213,288 @@ idsl_hash_set_name (idsl_hash_t ht, const char* name) {
 	return ht;
 }
 
+idsl_element_t
+idsl_hash_insert (idsl_hash_t ht, void* value) {
+	unsigned short index;
+	hash_elemnet	he;
+	idsl_list_t		l;
 
+	assert (ht != NULL);
 
+	he = (hash_element) malloc (sizeof (struct _hash_element));
+	if (he == NULL) {
+		return NULL;
+	}
+
+	he->content = ht->alloc_func (value);
+	if (he->content == NULL) {
+		free (he);
+		return NULL;
+	}
+
+	he->key = ht->key_func (he->content);
+
+	index = ht->hash_func (he->key) % ht->lists_count;
+	l = ht->lists[index];
+
+	if (ht->lists_max_size != 0 && idsl_list_get_size(l) + 1 > ht->lists_max_size) {
+		if (idsl_hash_modify (ht, ht->lists_count * 2 + 1, ht->lists_max_size * 2) != NULL) {
+			index = ht->hash_func (he->key) % ht->lists_count;
+			l = ht->lists[index];
+		}
+	}
+
+	if (idsl_list_insert_head (l, he) == NULL) {
+		ht->free_func (he->content);
+		free (ht);
+		return NULL;
+	}
+
+	return he->content;
+}
+
+idsl_element_t
+idsl_hash_remove (idsl_hash_t ht, const char* key) {
+	unsigned short index;
+	hash_element he;
+	idsl_element_t e;
+
+	index = ht->hash_func (key) % ht->lists_count;
+
+	he = (hash_element) idsl_list_remove (ht->lists[index], search_element_by_key, (void*)key);
+
+	if (he == NULL) {
+		return NULL;
+	}
+
+	e = he->content;
+	free (he);
+
+	return e;
+}
+
+idsl_hash_t
+idsl_hash_delete (idsl_hash_t ht, const char* key) {
+	idsl_element_t e;
+
+	e = idsl_hash_remove (ht, key);
+	if (e == NULL) {
+		return NULL;
+	}
+
+	ht->free_func (e);
+
+	return ht;
+}
+
+idsl_hash_t
+idsl_hash_modify (idsl_hash_t ht, unsigned short new_size, unsigned short new_max_lists_size) {
+	unsigned short i;
+	unsigned short j;
+	idsl_list_t *lists;
+
+	assert (ht != NULL);
+
+	lists = (idsl_list_t*) malloc (new_size * sizeof (idsl_list_t));
+	if (lists == NULL) {
+		return NULL;
+	}
+
+	for (i = 0; i < new_size; i++) {
+		for (j = 0; j < i; j++) {
+			idsl_list_free (lists[j]);
+		}
+
+		free (lists);
+		return NULL;
+	}
+
+	for (i = 0; i < ht->lists_count; i++) {
+		idsl_list_t l = ht->lists_count[i];
+		idsl_list_cursor_t c = idsl_list_cursor_alloc (l);
+		hash_element he;
+
+		for (idsl_list_cursor_move_to_head (c); (he = idsl_list_cursor_get_content (c)); idsl_list_cursor_step_forward (c)) {
+			unsigned short index = ht->hash_func (he->key) % new_size;
+			idsl_list_t l2 = lists[index];
+
+			if (idsl_list_insert_head (l2, he) == NULL) {
+				return NULL;
+			}
+		}
+		idsl_list_cursor_free (c);
+	}
+
+	for (i = 0; i < ht->lists_count; i++) {
+		idsl_list_flush (ht->lists[i]);
+	}
+	free (ht->lists);
+	
+	ht->lists = lists;
+	ht->lists_count = new_size;
+	ht->lists_max_size = new_max_lists_size;
+
+	return ht;
+}
+
+idsl_element_t
+idsl_hash_search (const idsl_hash_t ht, const char* key) {
+	unsigned short index;
+	hash_element_t he;
+
+	index = ht->hash_func (key) % ht->lists_count;
+
+	he = (hash_element) idsl_list_search (ht->list[index], search_element_by_key, (void*) key);
+	if (he == NULL) {
+		return NULL;
+	}
+
+	return he->content;
+}
+
+void
+idsl_hash_map (const idsl_hash_t ht, idsl_write_func_t f, FILE* file, void* user_data) {
+	unsigned short i;
+	struct _infos infos;
+	
+	infos.f = (hash_fct_ptr_t) f;
+	infos.d = user_data;
+	infos.e = NULL;
+
+	for (i = 0; i < ht->lists_count; i++) {
+		if (idsl_list_get_size (ht->lists[i]) == 0) {
+			continue;
+		}
+
+		if (idsl_list_map_forward (ht->lists[i], local_map_f, (void*) &infos) != NULL) {
+			return infos.e;
+		}
+	}
+
+	return NULL;
+}
+
+void
+idsl_hash_write (const idsl_hash_t ht, idsl_write_func_t f, FILE* file, void* user_data) {
+	unsigned short i;
+	struct _infos infos;
+
+	infos.f = (hash_fct_ptr_t) f;
+	infos.d = (void*)file;
+	infos.ud = user_data;
+
+	for (i = 0; i < ht->lists_count; i++) {
+		if (idsl_list_get_size (ht->lists[i]) != 0) {
+			idsl_list_write (ht->lists[i], local_write_f, file, (void*) &infos);
+		}
+	}
+}
+
+void
+idsl_hash_write_xml (const idsl_hash_t ht, idsl_write_func_t f, FILE* file, void* user_data) {
+	unsigned short i;
+	struct _infos infos;
+
+	infos.f = (hash_fct_ptr_t) f;
+	infos.d = (void*) file;
+	infos.ud = user_data;
+
+	fprintf (file, "<IDSL_HASH REF=\"%p\" NAME=\"%p\" SIZE=\"%ld\" ENTRIES_COUNT=\"%d\">\n", (void*) ht, ht->name == NULL ? "" : ht->name, idsl_hash_get_size (ht), ht->lists_count);
+
+	for (i = 0; i < ht->lists_count; i++) {
+		if (idsl_list_get_size (ht->lists[i] != 0)) {
+			fprintf (file, "<IDSL_HASH_ENTRY VALE=\"%d\">\n", i);
+			idsl_list_dump (ht->lists[i], local_write_xml_f, file, (void*) &infos);
+			fprintf (file, "</IDSL_HASH_ENTRY>\n");
+		}
+	}
+
+	fprintf (file, "</IDSL_HASH>\n");
+}
+
+void
+idsl_hash_dump (const idsl_hash_t ht, idsl_write_func_t f, FILE* file, void* user_data) {
+	unsigned short i;
+	struct _infos infos;
+
+	infos.f = (hash_fct_ptr_t) f;
+	infos.d = file;
+	infos.ud = user_data;
+
+	fprintf (file, "<IDSL_HASH REF=\"%p\" NAME=\"%s\" SIZE=\"%ld\" ENTRIES_COUNT=\"%d\" MAX_LISTS_SIZE=\"%d\">\n", (void*)ht, ht->name == NULL ? "" : ht->name, idsl_hash_get_size (ht), ht->lists_count, ht->lists_max_size);
+
+	for (i = 0; i < ht->lists_count; i++) {
+		fprintf (file, "<ISDL_HASH_ENTRY VALUE=\"%d\">\n", i);
+		idsl_list_dump (ht->lists[i], local_write_xml_f, file, (void*) &infos);
+		fprintf (file, "</IDSL_HASH_ENTRY>\n");
+	}
+	fprintf (file, "</IDSL_HASH>\n");
+}
+
+/**************************/
+/* Private func           */
+/**************************/
+
+static idsl_element_t
+default_alloc (void* e) {
+	return e;
+}
+
+static void
+default_free (idsl_element_t e) {
+	;
+}
+
+static const char*
+default_key (idsl_element_t e) {
+	return (const char*)e;
+}
+
+static long int
+search_element_by_key (idsl_element_t e, void* key) {
+	hash_element he = (hash_element) e;
+
+	return strcmp (he->key, (const char*)key);
+}
+
+static int
+local_map_f (idsl_element_t e, idsl_location_f location, void* user_data) {
+	hash_element he = (hash_element) e;
+	struct _infos* infos = (struct _infos*) user_data;
+	idsl_map_func_t map = (idsl_map_func_t) (infos->f);
+
+	infos->e = he->content;
+	
+	return map (he->content, IDSL_LOCATION_UNDEF, infos->d);
+}
+
+static void
+local_write_f (idsl_element_t e, FILE* file, idls_location_t location, void* user_data) {
+	hash_element he = (hash_element) e;
+	struct _infos* infos = (struct _infos*) user_data;
+	idsl_write_func_t write = (idsl_write_func_t) (infos->f);
+
+	write (he->content, file, IDSL_LOCATION_UNDEF, infos->ud);
+}
+
+static void
+local_write_xml_f (idsl_element_t e, FILE* file, idls_location_t location, void* user_data) {
+	hash_element he = (hash_element) e;
+	struct _infos* infos = (struct _infos*) user_data;
+	idsl_write_func_t write = (idsl_write_func_t) (infos->f);
+
+	fprintf (file, "\<CONTENT KEY=\"%s\">", he->key);
+	write (he->content, file, IDSL_LOCATION_UNDEF, infos->ud);
+	fprintf (file, "</CONTENT>\n");
+}
+
+static int
+destroy_element (idsl_element_t e, idsl_location_t location, void* user_infos) {
+	idsl_hash_t ht = (idsl_hash_t) user_infos;
+	hash_element he = (hash_element) e;
+	
+	(ht->free_func) (he->content);
+	free (he);
+
+	return IDSL_MAP_CONT;
+}
