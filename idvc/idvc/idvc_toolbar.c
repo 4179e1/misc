5e6fc4ff48b1d4d75dd2423c4650bbc03b4abcad
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2009-10-22 17:53:38 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@69 906dfecc-7149-11de-8b73-c1edde54a3c7

--- idvc/idvc/idvc_toolbar.c
+++ idvc/idvc/idvc_toolbar.c
@@ -2,42 +2,100 @@
 #include "idvc.h"
 #include "idvc_toolbar.h"
 
+enum _idvc_tool_button_type
+{
+	IDVC_TOOL_BUTTON_NORMAL = 0,
+	IDVC_TOOL_BUTTON_STOCK,
+	IDVC_TOOL_BUTTON_SEP,
+	IDVC_TOOL_BUTTON_END,
+};
+typedef enum _idvc_tool_button_type IdvcToolButtonType;
+
+struct _idvc_toolbutton
+{
+	IdvcToolButtonType type;
+	GtkWidget *icon_widget;
+	const char *label;
+	GCallback callback_func;
+};
+typedef struct _idvc_toolbutton IdvcToolButton;
+
+static void idvc_toolbar_build (GtkWidget *toolbar, IdvcToolButton *buttonlist, Idvc *idvc_data);
+
+/* a test func for toolbutton callback */
+static void idvc_toolbutton_callback_testing (GtkToolButton *toolbutton, Idvc *idvc_data)
+{
+	g_message ("%s is clicked", gtk_widget_get_name (GTK_WIDGET (toolbutton)));
+}
+
+/* create the toolbar and buttons on it */
 GtkWidget *idvc_toolbar_new (Idvc *idvc_data)
 {
 	GtkWidget *toolbar;
-	GtkToolItem *bt_add;
-	GtkToolItem *bt_directory;
-	GtkToolItem *bt_remove;
-	GtkToolItem *bt_clear;
-	GtkToolItem *bt_hide;
-	GtkToolItem *bt_quit;
-	GtkToolItem *bt_sep;
-
 	toolbar = gtk_toolbar_new ();
 
-	bt_add = gtk_tool_button_new_from_stock (GTK_STOCK_ADD);
-	gtk_toolbar_insert (GTK_TOOLBAR (toolbar), bt_add, -1);
+	IdvcToolButton buttonlist[] = 
+	{
+		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_ADD, G_CALLBACK (idvc_toolbutton_callback_testing)},
+		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_REMOVE, G_CALLBACK (idvc_toolbutton_callback_testing)},
+		{IDVC_TOOL_BUTTON_SEP,NULL, NULL, NULL},
+		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_DIRECTORY, NULL},
+		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_CLEAR, NULL},
+		{IDVC_TOOL_BUTTON_SEP, NULL, NULL, NULL},
+		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_CLOSE, NULL},
+		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_QUIT, NULL},
+		{IDVC_TOOL_BUTTON_END, NULL, NULL, NULL},
+	};
 
-	bt_remove = gtk_tool_button_new_from_stock (GTK_STOCK_REMOVE);
-	gtk_toolbar_insert (GTK_TOOLBAR (toolbar), bt_remove, -1);
-
-	bt_sep = gtk_separator_tool_item_new ();
-	gtk_toolbar_insert (GTK_TOOLBAR (toolbar), bt_sep, -1);
-
-	bt_directory = gtk_tool_button_new_from_stock (GTK_STOCK_DIRECTORY);
-	gtk_toolbar_insert (GTK_TOOLBAR (toolbar), bt_directory, -1);
-	
-	bt_clear = gtk_tool_button_new_from_stock (GTK_STOCK_CLEAR);
-	gtk_toolbar_insert (GTK_TOOLBAR (toolbar), bt_clear, -1);
-
-	bt_sep = gtk_separator_tool_item_new ();
-	gtk_toolbar_insert (GTK_TOOLBAR (toolbar), bt_sep, -1);
-
-	bt_hide = gtk_tool_button_new_from_stock (GTK_STOCK_CLOSE);
-	gtk_toolbar_insert (GTK_TOOLBAR (toolbar), bt_hide, -1);
-
-	bt_quit = gtk_tool_button_new_from_stock (GTK_STOCK_QUIT);
-	gtk_toolbar_insert (GTK_TOOLBAR (toolbar), bt_quit, -1);
+	idvc_toolbar_build (toolbar, buttonlist, idvc_data);
 
 	return toolbar;
+}
+
+static void idvc_toolbar_build (GtkWidget *toolbar, IdvcToolButton *buttonlist, Idvc *idvc_data)
+{
+	IdvcToolButton *ptr = buttonlist;
+	GtkToolItem *button;
+
+	while (ptr->type != IDVC_TOOL_BUTTON_END)
+	{
+		switch (ptr->type)
+		{
+			case IDVC_TOOL_BUTTON_NORMAL:
+			case IDVC_TOOL_BUTTON_STOCK:
+				{
+					if (ptr->type == IDVC_TOOL_BUTTON_NORMAL)
+					{
+						/* TODO: not knowing how to do */;
+						button = gtk_tool_button_new (ptr->icon_widget, ptr->label);
+					}
+					else {
+						button = gtk_tool_button_new_from_stock (ptr->label);
+					}
+
+					if (ptr->callback_func != NULL)
+					{
+						g_signal_connect (GTK_TOOL_BUTTON (button), "clicked", ptr->callback_func, idvc_data);
+					}
+
+					gtk_toolbar_insert (GTK_TOOLBAR (toolbar), button, -1);
+
+					break;
+				}
+			case IDVC_TOOL_BUTTON_SEP:
+				{
+					button = gtk_separator_tool_item_new ();
+					gtk_toolbar_insert (GTK_TOOLBAR (toolbar), button, -1);
+					break;
+				}
+			case IDVC_TOOL_BUTTON_END:
+			default:
+				{
+					g_warning ("Here should not be reached");
+					break;
+				}
+		}
+		ptr++;
+	}
+
 }
