aedd38b1abb6756aa87dc5465d233175e34c52c3
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2009-10-22 18:35:02 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@70 906dfecc-7149-11de-8b73-c1edde54a3c7

--- idvc/idvc/idvc_toolbar.c
+++ idvc/idvc/idvc_toolbar.c
@@ -2,25 +2,25 @@
 #include "idvc.h"
 #include "idvc_toolbar.h"
 
-enum _idvc_tool_button_type
+enum _tool_button_type
 {
-	IDVC_TOOL_BUTTON_NORMAL = 0,
-	IDVC_TOOL_BUTTON_STOCK,
-	IDVC_TOOL_BUTTON_SEP,
-	IDVC_TOOL_BUTTON_END,
+	TOOL_BUTTON_NORMAL = 0,
+	TOOL_BUTTON_STOCK,
+	TOOL_BUTTON_SEP,
+	TOOL_BUTTON_END,
 };
-typedef enum _idvc_tool_button_type IdvcToolButtonType;
+typedef enum _tool_button_type ToolButtonType;
 
-struct _idvc_toolbutton
+struct _tool_button
 {
-	IdvcToolButtonType type;
+	ToolButtonType type;
 	GtkWidget *icon_widget;
 	const char *label;
 	GCallback callback_func;
 };
-typedef struct _idvc_toolbutton IdvcToolButton;
+typedef struct _tool_button ToolButton;
 
-static void idvc_toolbar_build (GtkWidget *toolbar, IdvcToolButton *buttonlist, Idvc *idvc_data);
+static void idvc_toolbar_build (GtkWidget *toolbar, ToolButton *buttonlist, Idvc *idvc_data);
 
 /* a test func for toolbutton callback */
 static void idvc_toolbutton_callback_testing (GtkToolButton *toolbutton, Idvc *idvc_data)
@@ -28,23 +28,25 @@ static void idvc_toolbutton_callback_testing (GtkToolButton *toolbutton, Idvc *i
 	g_message ("%s is clicked", gtk_widget_get_name (GTK_WIDGET (toolbutton)));
 }
 
+#define TOOL_BUTTON_SIZE 26
+
 /* create the toolbar and buttons on it */
 GtkWidget *idvc_toolbar_new (Idvc *idvc_data)
 {
 	GtkWidget *toolbar;
 	toolbar = gtk_toolbar_new ();
 
-	IdvcToolButton buttonlist[] = 
+	ToolButton buttonlist[] = 
 	{
-		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_ADD, G_CALLBACK (idvc_toolbutton_callback_testing)},
-		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_REMOVE, G_CALLBACK (idvc_toolbutton_callback_testing)},
-		{IDVC_TOOL_BUTTON_SEP,NULL, NULL, NULL},
-		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_DIRECTORY, NULL},
-		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_CLEAR, NULL},
-		{IDVC_TOOL_BUTTON_SEP, NULL, NULL, NULL},
-		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_CLOSE, NULL},
-		{IDVC_TOOL_BUTTON_STOCK, NULL, GTK_STOCK_QUIT, NULL},
-		{IDVC_TOOL_BUTTON_END, NULL, NULL, NULL},
+		{TOOL_BUTTON_STOCK, NULL, GTK_STOCK_ADD, G_CALLBACK (idvc_toolbutton_callback_testing)},
+		{TOOL_BUTTON_STOCK, NULL, GTK_STOCK_REMOVE, G_CALLBACK (idvc_toolbutton_callback_testing)},
+		{TOOL_BUTTON_SEP,NULL, NULL, NULL},
+		{TOOL_BUTTON_NORMAL, gtk_image_new_from_stock (GTK_STOCK_DIRECTORY, TOOL_BUTTON_SIZE), "Add Directory", NULL},
+		{TOOL_BUTTON_STOCK, NULL, GTK_STOCK_CLEAR, NULL},
+		{TOOL_BUTTON_SEP, NULL, NULL, NULL},
+		{TOOL_BUTTON_STOCK, NULL, GTK_STOCK_CLOSE, NULL},
+		{TOOL_BUTTON_STOCK, NULL, GTK_STOCK_QUIT, NULL},
+		{TOOL_BUTTON_END, NULL, NULL, NULL},
 	};
 
 	idvc_toolbar_build (toolbar, buttonlist, idvc_data);
@@ -52,22 +54,22 @@ GtkWidget *idvc_toolbar_new (Idvc *idvc_data)
 	return toolbar;
 }
 
-static void idvc_toolbar_build (GtkWidget *toolbar, IdvcToolButton *buttonlist, Idvc *idvc_data)
+static void idvc_toolbar_build (GtkWidget *toolbar, ToolButton *buttonlist, Idvc *idvc_data)
 {
-	IdvcToolButton *ptr = buttonlist;
+	ToolButton *ptr = buttonlist;
 	GtkToolItem *button;
 
-	while (ptr->type != IDVC_TOOL_BUTTON_END)
+	while (ptr->type != TOOL_BUTTON_END)
 	{
 		switch (ptr->type)
 		{
-			case IDVC_TOOL_BUTTON_NORMAL:
-			case IDVC_TOOL_BUTTON_STOCK:
+			case TOOL_BUTTON_NORMAL:
+			case TOOL_BUTTON_STOCK:
 				{
-					if (ptr->type == IDVC_TOOL_BUTTON_NORMAL)
+					if (ptr->type == TOOL_BUTTON_NORMAL)
 					{
-						/* TODO: not knowing how to do */;
 						button = gtk_tool_button_new (ptr->icon_widget, ptr->label);
+						gtk_tool_button_set_label (GTK_TOOL_BUTTON(button), ptr->label);
 					}
 					else {
 						button = gtk_tool_button_new_from_stock (ptr->label);
@@ -78,17 +80,19 @@ static void idvc_toolbar_build (GtkWidget *toolbar, IdvcToolButton *buttonlist, 
 						g_signal_connect (GTK_TOOL_BUTTON (button), "clicked", ptr->callback_func, idvc_data);
 					}
 
+					gtk_widget_set_tooltip_text (GTK_WIDGET (button), ptr->label);
+
 					gtk_toolbar_insert (GTK_TOOLBAR (toolbar), button, -1);
 
 					break;
 				}
-			case IDVC_TOOL_BUTTON_SEP:
+			case TOOL_BUTTON_SEP:
 				{
 					button = gtk_separator_tool_item_new ();
 					gtk_toolbar_insert (GTK_TOOLBAR (toolbar), button, -1);
 					break;
 				}
-			case IDVC_TOOL_BUTTON_END:
+			case TOOL_BUTTON_END:
 			default:
 				{
 					g_warning ("Here should not be reached");
