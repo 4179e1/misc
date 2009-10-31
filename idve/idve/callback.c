3c0b7c9957890f7d3f7934967bdaac0612193389
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2009-10-31 07:58:29 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@76 906dfecc-7149-11de-8b73-c1edde54a3c7

--- idve/idve/callback.c
+++ idve/idve/callback.c
@@ -2,11 +2,24 @@
 #include "idve.h"
 #include "callback.h"
 
+static GSList *get_path (GtkWindow *parent, GtkFileChooserAction action);
+
 G_MODULE_EXPORT 
 void on_liststore_insert (gpointer *widget, Idve *idve)
 {
-	idve_liststore_insert (idve, "testing msg");
+	GSList *pathlist;
+
+	pathlist = get_path (GTK_WINDOW (idve_get_window (idve)),
+			GTK_FILE_CHOOSER_ACTION_OPEN);
+	
+	if (pathlist != NULL)
+	{
+	//	idve_liststore_insert (idve, path);
+	//	g_free (path);
+		g_slist_free (pathlist);
+	}
 }
+
 
 G_MODULE_EXPORT 
 void on_liststore_clear (gpointer *widget, Idve *idve)
@@ -25,4 +38,27 @@ G_MODULE_EXPORT
 void on_statusbar_toggle (gpointer *widget, Idve *idve)
 {
 	idve_statusbar_toggle (idve);
+}
+
+static GSList *get_path (GtkWindow *parent, GtkFileChooserAction action)
+{
+	GtkWidget *chooser;
+//	gchar *path = NULL;
+	GSList *pathlist = NULL;
+
+	chooser = gtk_file_chooser_dialog_new ("Select Path...",
+			parent,
+			action,
+			GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL, /* button_text, response_id */
+			GTK_STOCK_OK, GTK_RESPONSE_OK,
+			NULL);
+	gtk_file_chooser_set_select_multiple (GTK_FILE_CHOOSER (chooser), TRUE);
+
+	if (gtk_dialog_run (GTK_DIALOG (chooser)) == GTK_RESPONSE_OK)
+	{
+		pathlist = gtk_file_chooser_get_filenames (GTK_FILE_CHOOSER (chooser));
+	}
+
+	gtk_widget_destroy (chooser);
+	return pathlist;
 }
