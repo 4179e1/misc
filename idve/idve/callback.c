d235917629c18dbb8072f62515d3dd46cae88cfe
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2009-10-31 10:35:17 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@77 906dfecc-7149-11de-8b73-c1edde54a3c7

--- idve/idve/callback.c
+++ idve/idve/callback.c
@@ -14,12 +14,16 @@ void on_liststore_insert (gpointer *widget, Idve *idve)
 	
 	if (pathlist != NULL)
 	{
-	//	idve_liststore_insert (idve, path);
-	//	g_free (path);
+		GSList *ptr = pathlist;
+		while (ptr != NULL)
+		{
+			idve_liststore_insert (idve, (gchar *)ptr->data);
+			g_free (ptr->data);
+			ptr = g_slist_next (ptr);
+		}
 		g_slist_free (pathlist);
 	}
 }
-
 
 G_MODULE_EXPORT 
 void on_liststore_clear (gpointer *widget, Idve *idve)
@@ -40,19 +44,26 @@ void on_statusbar_toggle (gpointer *widget, Idve *idve)
 	idve_statusbar_toggle (idve);
 }
 
+/* Private func */
 static GSList *get_path (GtkWindow *parent, GtkFileChooserAction action)
 {
 	GtkWidget *chooser;
-//	gchar *path = NULL;
+	GtkFileFilter *filter;
 	GSList *pathlist = NULL;
 
-	chooser = gtk_file_chooser_dialog_new ("Select Path...",
+	chooser = gtk_file_chooser_dialog_new ("Select...",
 			parent,
 			action,
-			GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL, /* button_text, response_id */
+			GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL, /* button, response_id */
 			GTK_STOCK_OK, GTK_RESPONSE_OK,
 			NULL);
 	gtk_file_chooser_set_select_multiple (GTK_FILE_CHOOSER (chooser), TRUE);
+	
+/* add a filter, we only handle mp3 files */	
+	filter = gtk_file_filter_new ();
+	gtk_file_filter_set_name (filter, "mp3 Files");
+	gtk_file_filter_add_pattern (filter, "*.mp3");
+	gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (chooser), filter);
 
 	if (gtk_dialog_run (GTK_DIALOG (chooser)) == GTK_RESPONSE_OK)
 	{
