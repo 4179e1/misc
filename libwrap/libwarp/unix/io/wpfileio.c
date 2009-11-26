2d7525ac214bb1455a3a7dc0cf006c2beefbc321
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2009-11-26 15:26:45 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@119 906dfecc-7149-11de-8b73-c1edde54a3c7

--- libwrap/libwarp/unix/io/wpfileio.c
+++ libwrap/libwarp/unix/io/wpfileio.c
@@ -3,6 +3,30 @@
 #include "wpunixio.h"
 #include "../../base/wpbase.h"
 
+int wp_access (const char *pathname, int mode)
+{
+	int n;
+	if ((n = access (pathname, mode)) == -1)
+		wp_error_sys_info ("access error");
+	return n;
+}
+
+int wp_chmod (const char *pathname, mode_t mode)
+{
+	int n;
+	if ((n = chmod (pathname, mode)) == -1)
+		wp_error_sys_warning ("chmod error");
+	return n;
+}
+
+int wp_chown (const char *pathname, uid_t owner, gid_t group)
+{
+	int n;
+	if ((n = chown (pathname, owner, group)) == -1)
+		wp_error_sys_warning ("chown error");
+	return n;
+}
+
 int wp_close (int filedes)
 {
 	int rt;
@@ -35,13 +59,25 @@ int wp_dup2 (int filedes, int filedes2)
 	return fd;
 }
 
-int wp_fdatasync (int filedes)
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_fchmod (int filedes, mode_t mode)
 {
-	int rt;
-	if ((rt = fdatasync (filedes)) == -1)
-		wp_error_sys_warning ("fdatasync error");
-	return rt;
+	int n;
+	if ((n = fchmod (filedes, mode)) == -1)
+		wp_error_sys_warning ("fchmod error");
+	return n;
 }
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED */
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_fchown (int filedes, uid_t user, gid_t group)
+{
+	int n;
+	if ((n = fchown (filedes, user, group)) == -1)
+		wp_error_sys_warning ("fchown error");
+	return n;
+}
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED */
 
 int wp_fcntl (int filedes, int cmd, int arg)
 {
@@ -51,7 +87,44 @@ int wp_fcntl (int filedes, int cmd, int arg)
 	return rt;
 }
 
-int wp_fsync (int filedes);
+#if defined __USE_POSIX199309 || defined __USE_UNIX98
+int wp_fdatasync (int filedes)
+{
+	int rt;
+	if ((rt = fdatasync (filedes)) == -1)
+		wp_error_sys_warning ("fdatasync error");
+	return rt;
+}
+#endif /* __USE_POSIX199309 */
+
+int wp_fstat (int filedes, struct stat *buf)
+{
+	int n;
+	if ((n = fstat (filedes, buf)) == -1)
+		wp_error_sys_warning ("fstat error");
+	return n;
+}
+
+#if defined __USE_BSD || defined __USE_XOPEN || defined __USE_XOPEN2K
+int wp_fsync (int filedes)
+{
+	int n;
+	if ((n = fsync (filedes)) == -1)
+		wp_error_sys_warning ("fsysnc error");
+	return n;
+}
+#endif /* Use BSD || X/Open || Unix98.  */
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int ftruncate (int filedes, off_t length)
+{
+	int n;
+	if ((n = ftuncate (filedes, length)) == -1)
+		wp_error_sys_warning ("ftruncate error");
+	return n;
+}
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED */
+
 off_t wp_lseek (int filedes, off_t offset, int whence)
 {
 	off_t pos;
@@ -59,6 +132,26 @@ off_t wp_lseek (int filedes, off_t offset, int whence)
 		wp_error_sys_warning ("lseek error");
 	return pos;
 }
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_lchown (const char *pathname, uid_t owner, gid_t group)
+{
+	int n;
+	if ((n = lchown (pathname, owner, group)) == -1)
+		wp_error_sys_warning ("lchown error");
+	return n;
+}
+#endif /* Use BSD || X/Open Unix.  */
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED || defined __USE_XOPEN2K
+int wp_lstat (const char *restrict pathname, struct stat *restrict buf)
+{
+	int n;
+	if ((n = lstat (pathname, buf)) == -1)
+		wp_error_sys_warning ("lstat error");
+	return n;
+}
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED || defined __USE_XOPEN2K */
 
 int wp_ioctl (int filedes, int request, void *arg)
 {
@@ -81,5 +174,31 @@ ssize_t wp_read (int filedes, void *buf, size_t nbytes)
 	ssize_t n;
 	if ((n = read (filedes, buf, nbytes)) == -1)
 		wp_error_sys_warning ("read error");
+	return n;
+}
+
+int wp_stat (const char *restrict pathname, struct stat *restrict buf)
+{
+	int n;
+	if ((n = stat (pathname, buf)) == -1)
+		wp_error_sys_warning ("stat error");
+	return n;
+}
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_truncate (const char *pathname, off_t length)
+{
+	int n;
+	if ((n = truncate (pathname, length)) == -1)
+		wp_error_sys_warning ("truncate error");
+	return n;
+}
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED */
+
+ssize_t wp_write (int filedes, const void *buf, size_t nbytes)
+{
+	ssize_t n;
+	if ((n = write (filedes, buf, nbytes)) == -1)
+		wp_error_sys_warning ("write error");
 	return n;
 }
--- libwrap/libwarp/unix/io/wpunixio.h
+++ libwrap/libwarp/unix/io/wpunixio.h
@@ -2,27 +2,70 @@
 #define _WPUNIXIO_H
 
 #include <sys/ioctl.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdbool.h>
 
 /***************************************
- * wpfileio
+ * wp file io
  **************************************/
+
+int wp_access (const char *pathname, int mode);
+int wp_chmod (const char *pathname, mode_t mode);
+int wp_chown (const char *pathname, uid_t owner, gid_t group);
 int wp_close (int filedes);
 int wp_creat (const char *pathname, mode_t mode);
 int wp_dup (int filedes);
 int wp_dup2 (int filedes, int filedes2);
-int wp_fdatasync (int filedes);
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_fchmod (int filedes, mode_t mode);
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED */
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_fchown (int filedes, uid_t user, gid_t group);
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED */
+
 int wp_fcntl (int filedes, int cmd, int arg);
+
+#if defined __USE_POSIX199309 || defined __USE_UNIX98
+int wp_fdatasync (int filedes);
+#endif /* __USE_POSIX199309 */
+
+int wp_fstat (int filedes, struct stat *buf);
+
+#if defined __USE_BSD || defined __USE_XOPEN || defined __USE_XOPEN2K
 int wp_fsync (int filedes);
+#endif /* Use BSD || X/Open || Unix98.  */
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_ftruncate (int filedes, off_t length);
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED */
+
 int wp_ioctl (int filedes, int request, void *arg);
 off_t wp_lseek (int filedes, off_t offset, int whence);
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_lchown (const char *pathname, uid_t owenr, gid_t group);
+#endif /* Use BSD || X/Open Unix.  */
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED || defined __USE_XOPEN2K
+int wp_lstat (const char *restrict pathname, struct stat *restrict buf);
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED || defined __USE_XOPEN2K */
+
 int wp_open (const char *pathname, int oflag, mode_t mode);
 ssize_t wp_read (int filedes, void *buf, size_t nbytes);
+int wp_stat (const char *restrict pathname, struct stat *restrict buf);
+
+#if defined __USE_BSD || defined __USE_XOPEN_EXTENDED
+int wp_truncate (const char *pathname, off_t length);
+#endif /* __USE_BSD || defined __USE_XOPEN_EXTENDED */
+
+ssize_t wp_write (int filedes, const void *buf, size_t nbytes);
 
 /**************************************
- * wpfileutilities
+ * wp file utilities
  *************************************/
 int wp_fd_create (const char *pathname, mode_t mode);
 int wp_fd_alloc (const char *pathname, mode_t mode, off_t offset);
