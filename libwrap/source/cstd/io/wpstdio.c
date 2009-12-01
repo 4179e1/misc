29d920ec85e1aaa3264a9b14dd64846c6a0199b2
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2009-12-01 14:18:16 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@133 906dfecc-7149-11de-8b73-c1edde54a3c7

--- libwrap/source/cstd/io/wpstdio.c
+++ libwrap/source/cstd/io/wpstdio.c
@@ -2,11 +2,132 @@
 #include "wpstdio.h"
 #include "wpbase.h"
 
+int wp_fclose (FILE *fp)
+{
+	int n;
+	if ((n = fclose (fp)) == EOF)
+		wp_error_warning ("fclose error");
+	return n;
+}
+
+FILE *wp_fdopen (int filedes, const char *type)
+{
+	FILE *file;
+	if ((file = fdopen (filedes, type)) == NULL)
+		wp_error_warning ("fdopen error");
+	return file;
+}
+
+int wp_fgetc (FILE *fp)
+{
+	int n;
+	clearerr (fp);
+	n = fgetc (fp);
+	if (n == EOF && ferror (fp) == 0)
+		wp_error_warning ("fgetc error");
+	return n;
+}
+
+char *wp_fgets (char *buf, int n, FILE *fp)
+{
+	char *str;
+	if ((str = fgets (buf, n, fp)) == NULL)
+		wp_error_warning ("fgets error");
+	return str;
+}
+
+int wp_fputc (int c, FILE *fp)
+{
+	int n;
+	if ((n = fputc (c, fp)) == EOF)
+		wp_error_warning ("fputc error");
+	return n;
+}
+
+int wp_fputs (const char *str, FILE *fp)
+{
+	int n;
+	if ((n = fputs (str, fp)) == EOF)
+		wp_error_warning ("fputs error");
+	return n;
+}
+
+int wp_fflush (FILE *fp)
+{
+	int n;
+	if ((n = fflush (fp)) == EOF)
+		wp_error_warning ("fflush error");
+	return n;
+}
+
+FILE *wp_fopen (const char *path, const char *type)
+{
+	FILE *file;
+	if ((file = fopen (path, type)) == NULL)
+		wp_error_warning ("fopen error");
+	return file;
+}
+
+size_t wp_fread (void *ptr, size_t size, size_t nobj, FILE *fp)
+{
+	size_t n;
+
+	clearerr (fp);
+	n = fread (ptr, size, nobj, fp);
+	if (ferror (fp) == 0)
+		wp_error_warning ("fread error");
+	return n;
+}
+
+FILE *wp_freopen (const char *pathname, const char *type, FILE *fp)
+{
+	FILE *file;
+	if ((file = freopen (pathname, type, fp)) == NULL)
+		wp_error_warning ("freopen error");
+	return file;
+}
+
+int wp_getc (FILE *fp)
+{
+	int n;
+	clearerr (fp);
+	n = getc (fp);
+	if (n == EOF && ferror(fp) == 0)
+		wp_error_warning ("getc error");
+	return n;
+}
+
+int wp_getchar (void)
+{
+	int n;
+	clearerr (stdin);
+	n = getchar ();
+	if (n == EOF && ferror (stdin) == 0)
+		wp_error_warning ("getchar error");
+	return n;
+}
+
+int wp_putc (int c, FILE *fp)
+{
+	int n;
+	if ((n = putc (c, fp)) == EOF)
+		wp_error_warning ("putc error");
+	return n;
+}
+
+int wp_putchar (int c)
+{
+	int n;
+	if ((n = putchar (c)) == EOF)
+		wp_error_warning ("putchar error");
+	return n;
+}
+
 int wp_remove (const char *path)
 {
 	int n;
 	if ((n = remove (path)) == -1)
-		wp_error_sys_warning ("remove error");
+		wp_error_warning ("remove error");
 	return n;
 }
 
@@ -14,6 +135,32 @@ int wp_rename (const char *oldname, const char *newname)
 {
 	int n;
 	if ((n = rename (oldname, newname)) == -1)
-		wp_error_sys_warning ("rename error");
+		wp_error_warning ("rename error");
+	return n;
+}
+
+int wp_setvbuf (FILE *fp, char *buf, int mode, size_t size)
+{
+	int n;
+	if ((n = setvbuf (fp, buf, mode, size)) != 0)
+		wp_error_warning ("setvbuf error");
+	return n;
+}
+
+int wp_ungetc (int c, FILE *fp)
+{
+	int n;
+	if ((n = ungetc (c, fp)) == EOF)
+		wp_error_warning ("ungetc error");
+	return n;
+}
+
+size_t wp_fwrite (const void *ptr, size_t size, size_t nobj, FILE *fp)
+{
+	int n;
+	clearerr (fp);
+	n = fwrite (ptr, size, nobj, fp);
+	if (ferror (fp) == 0)
+		wp_error_warning ("fwrite error");
 	return n;
 }
--- libwrap/source/cstd/io/wpstdio.h
+++ libwrap/source/cstd/io/wpstdio.h
@@ -1,7 +1,26 @@
 #ifndef _WPSTDIO_H
 #define _WPSTDIO_H
 
-int remove (const char *pathname);
-int rename (const char *oldname, const char *newname);
+#include <stdio.h>
+
+int wp_fclose (FILE *fp);
+FILE *wp_fdopen (int filedes, const char *type);
+int wp_fgetc (FILE *fp);
+char *wp_fgets (char *buf, int n, FILE *fp);
+int wp_fputc (int c, FILE *fp);
+int wp_fputs (const char *str, FILE *fp);
+int wp_fflush (FILE *fp);
+FILE *wp_fopen (const char *pathname, const char *type);
+size_t wp_fread (void *ptr, size_t size, size_t nobj, FILE *fp);
+FILE *wp_freopen (const char *pathname, const char *type, FILE *fp);
+int wp_getc (FILE *fp);
+int wp_getchar (void);
+int wp_putc (int c, FILE *fp);
+int wp_putchar (int c);
+int wp_remove (const char *pathname);
+int wp_rename (const char *oldname, const char *newname);
+int wp_setvbuf (FILE *fp, char *buf, int mode, size_t size);
+int wp_ungetc (int c, FILE *fp);
+size_t wp_fwrite (const void *ptr, size_t size, size_t nobj, FILE *fp);
 
 #endif /* _WPSTDIO_H */
