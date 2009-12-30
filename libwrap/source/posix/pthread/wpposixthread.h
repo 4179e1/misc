bce88a2546a9354b2e25ca224e3d13e40b01adc8
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2009-12-30 13:17:33 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@168 906dfecc-7149-11de-8b73-c1edde54a3c7

--- libwrap/source/posix/pthread/wpposixthread.h
+++ libwrap/source/posix/pthread/wpposixthread.h
@@ -12,8 +12,22 @@ int wp_pthread_detach (pthread_t tid);
 
 int wp_pthread_mutex_init (pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
 int wp_pthread_mutex_destroy (pthread_mutex_t *mutex);
-
 int wp_pthread_mutex_lock (pthread_mutex_t *mutex);
 int wp_pthread_mutex_trylock (pthread_mutex_t *mutex);
 int wp_pthread_mutex_unlock (pthread_mutex_t *mutex);
+
+int wp_pthread_rwlock_init (pthread_rwlock_t *rwlock, const pthread_rwlockattr_t *attr);
+int wp_pthread_rwlock_destroy (pthread_rwlock_t *rwlock);
+int wp_pthread_rwlock_rdlock (pthread_rwlock_t *rwlock);
+int wp_pthread_rwlock_wrlock (pthread_rwlock_t *rwlock);
+int wp_pthread_rwlock_unlock (pthread_rwlock_t *rwlock);
+int wp_pthread_rwlock_tryrdlock (pthread_rwlock_t *rwlock);
+int wp_pthread_rwlock_trywrlock (pthread_rwlock_t *rwlock);
+
+int wp_pthread_cond_init (pthread_cond_t *cond, pthread_condattr_t *buf);
+int wp_pthread_cond_destroy (pthread_cond_t *cond);
+int wp_pthread_cond_wait (pthread_cond_t *cond, pthread_mutex_t *mutex);
+int wp_pthread_cond_timedwait (pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *timeout);
+int wp_pthread_cond_signal (pthread_cond_t *cond);
+int wp_pthread_cond_broadcast (pthread_cond_t *cond);
 #endif /* _WPPOSIXTHREAD_H */
--- libwrap/source/posix/pthread/wppthread.c
+++ libwrap/source/posix/pthread/wppthread.c
@@ -73,3 +73,107 @@ int wp_pthread_mutex_unlock (pthread_mutex_t *mutex)
 		wp_error_warning ("pthread_mutex_unlock() error: %s", strerror(n));
 	return n;
 }
+
+int wp_pthread_rwlock_init (pthread_rwlock_t *rwlock, const pthread_rwlockattr_t *attr)
+{
+	int n;
+	if ((n = pthread_rwlock_init (rwlock, attr)) != 0)
+		wp_error_warning ("pthread_rwlock_init() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_rwlock_destroy (pthread_rwlock_t *rwlock)
+{
+	int n;
+	if ((n = pthread_rwlock_destroy (rwlock)) != 0)
+		wp_error_warning ("pthread_rwlock_destroy() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_rwlock_rdlock (pthread_rwlock_t *rwlock)
+{
+	int n;
+	if ((n = pthread_rwlock_rdlock (rwlock)) != 0)
+		wp_error_warning ("pthread_rwlock_rdlock() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_rwlock_wrlock (pthread_rwlock_t *rwlock)
+{
+	int n;
+	if ((n = pthread_rwlock_wrlock (rwlock)) != 0)
+		wp_error_warning ("pthread_rwlock_wrlock() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_rwlock_unlock (pthread_rwlock_t *rwlock)
+{
+	int n;
+	if ((n = pthread_rwlock_unlock (rwlock)) != 0)
+		wp_error_warning ("pthread_rwlock_unlock() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_rwlock_tryrdlock (pthread_rwlock_t *rwlock)
+{
+	int n;
+	if ((n = pthread_rwlock_tryrdlock (rwlock)) != 0)
+		wp_error_warning ("pthread_rwlock_tryrdlock() error: %s", strerror (n));
+	return n;
+}
+
+int wp_pthread_rwlock_trywrlock (pthread_rwlock_t *rwlock)
+{
+	int n;
+	if ((n = pthread_rwlock_trywrlock (rwlock)) != 0)
+		wp_error_warning ("pthread_rwlock_trywrlock() error: %s", strerror (n));
+	return n;
+}
+
+int wp_pthread_cond_init (pthread_cond_t *cond, pthread_condattr_t *buf)
+{
+	int n;
+	if ((n = pthread_cond_init (cond, buf)) != 0)
+		wp_error_warning ("pthread_cond_init() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_cond_destroy (pthread_cond_t *cond)
+{
+	int n;
+	if ((n = pthread_cond_destroy (cond)) != 0)
+		wp_error_warning ("pthread_cond_destroy() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_cond_wait (pthread_cond_t *cond, pthread_mutex_t *mutex)
+{
+	int n;
+	if ((n = pthread_cond_wait (cond, mutex)) != 0)
+		wp_error_warning ("pthread_cond_wait() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_cond_timedwait (pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *timeout)
+{
+	int n;
+	if ((n = pthread_cond_timedwait (cond, mutex, timeout)) != 0)
+		wp_error_warning ("pthread_cond_timedwait() error: %s", strerror (n));
+	return n;
+}
+
+int wp_pthread_cond_signal (pthread_cond_t *cond)
+{
+	int n;
+	if ((n = pthread_cond_signal (cond)) != 0)
+		wp_error_warning ("pthread_cond_signal() error: %s", strerror(n));
+	return n;
+}
+
+int wp_pthread_cond_broadcast (pthread_cond_t *cond)
+{
+	int n;
+	if ((n = pthread_cond_broadcast (cond)) != 0)
+		wp_error_warning ("pthread_cond_broadcast() error: %s", strerror(n));
+	return n;
+}
