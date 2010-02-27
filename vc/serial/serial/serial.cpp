c2e5749677677de6168bc7a4585b97ab26dfc8c4
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2010-02-27 14:39:25 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@192 906dfecc-7149-11de-8b73-c1edde54a3c7

---
+++ vc/serial/serial/serial.cpp
@@ -0,0 +1,58 @@
+// serial.cpp : Defines the entry point for the console application.
+//
+
+#include <stdlib.h>
+#include <Windows.h>
+#include <iostream>
+#include "stdafx.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	SetLastError (0);
+
+	printf ("Hello world\n");
+
+	HANDLE sh;
+
+	sh = CreateFile (TEXT("sdfafa"),
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if (sh == INVALID_HANDLE_VALUE)
+	{
+		LPTSTR error_string;
+		FormatMessage( 
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+			FORMAT_MESSAGE_FROM_SYSTEM | 
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			GetLastError(),
+			LANG_NEUTRAL, // Default language
+			(LPTSTR)&error_string,
+			0,
+			NULL 
+			);
+
+		wprintf (_T("%s\n"), error_string);
+
+		LocalFree( error_string );
+
+		system ("pause");
+		exit (0);
+	}
+	else
+	{
+		printf ("success\n");
+		CloseHandle (sh);
+	}
+
+	system ("pause");
+	return 0;
+}
+
