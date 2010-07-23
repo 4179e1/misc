a7ce1a6f7034e42a001d9be1477eab4a20c5feb7
4179e1 <4179e1@906dfecc-7149-11de-8b73-c1edde54a3c7>
2010-07-23 17:08:32 +0000

    git-svn-id: http://misc-codes.googlecode.com/svn/trunk@213 906dfecc-7149-11de-8b73-c1edde54a3c7

--- libwp/src/libwpunix_rio.h
+++ libwp/src/libwpunix_rio.h
@@ -4,4 +4,12 @@
 ssize_t wp_readn (int fd, void *ptr, size_t n);
 ssize_t wp_writen (int fd, void *ptr, size_t n);
 
+typedef struct _rio Rio;
+
+Rio *wp_rio_new (int fd);
+void wp_rio_free (Rio* rp);
+
+ssize_t wp_rio_readlineb (Rio *rp, void *usrbuf, size_t maxlen);
+ssize_t wp_rio_readnb (Rio *rp, void *usrbuf, size_t n);
+
 #endif /*_LIBWPUNIX_RIO_H */
