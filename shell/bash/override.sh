b6054255f036a6e0acf3ef1c95be29758eb98b8f
lyre <lyre@poetpalace.org>
2011-03-06 15:30:07 +0800

    shell: add command override

---
+++ shell/bash/override.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+cd()
+{
+	echo "mycd"
+}
+
+#function cd
+cd
+
+#bash builtin cd
+command -v cd
+
+#bash builtin cd
+builtin cd
---
+++ shell/bash/override2.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+which which
+enable -n which which
