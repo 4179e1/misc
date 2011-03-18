d6f80de42ff946ad4abe898cd6a197cf3eac9b3c
Lyre <lyre@poetpalace.org>
2011-03-18 23:07:52 +0800

    network: add validation for echo reply

--- network/udpclient.c
+++ network/udpclient.c
@@ -7,11 +7,23 @@ void dg_cli (FILE *fp, int sockfd, const struct sockaddr *servaddr, socklen_t se
 {
 	int n;
 	char sendline[MAXLINE], recvline[MAXLINE + 1];
+	socklen_t len;
+	struct sockaddr_in *reply_addr;
+	char address[INET_ADDRSTRLEN];
+	
+
+	reply_addr = wp_malloc (servlen);
 
 	while (wp_fgets (sendline, MAXLINE, fp) != NULL)
 	{
 		wp_sendto (sockfd, sendline, strlen (sendline), 0, servaddr, servlen);
-		n = wp_recvfrom (sockfd, recvline, MAXLINE, 0, NULL, NULL);
+		len = servlen;
+		n = wp_recvfrom (sockfd, recvline, MAXLINE, 0, (struct sockaddr *)reply_addr, &len);
+		if (len != servlen || memcmp (servaddr, reply_addr, len) != 0)
+		{
+			wp_debug ("reply from %s (ignored)", wp_inet_ntop (AF_INET, &(reply_addr->sin_addr), address, sizeof (address)));
+			continue;
+		}
 		recvline[n] = 0;
 		wp_fputs (recvline, stdout);
 	}
