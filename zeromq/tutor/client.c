c04f2e18f181527a8716f7d7812a5cc623bff20e
lyre <lyre@poetpalace.org>
2011-07-13 17:34:50 +0800

    zeromq: add example

---
+++ zeromq/tutor/client.c
@@ -0,0 +1,57 @@
+//
+// Hello World client
+// Connects REQ socket to tcp://localost:5555
+// Sends "Hello" to server, expects "World" back
+//
+#include <zmq.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static char *s_recv (void *socket)
+{
+	zmq_msg_t message;
+	zmq_msg_init (&message);
+	zmq_recv (socket, &message, 0);
+	int size = zmq_msg_size (&message);
+	char *string = malloc (size + 1);
+	memcpy (string, zmq_msg_data (&message), size);
+	zmq_msg_close (&message);
+	string[size] = 0;
+	return string;
+}
+
+int main (void)
+{
+	void *context = zmq_init (1);
+
+	// Socket to talk to server
+	printf ("Connecting to hello world server...\n");
+	void *requester = zmq_socket (context, ZMQ_REQ);
+	zmq_connect (requester, "tcp://localhost:5555");
+	char buf[100];
+
+	int request_nbr;
+	for (request_nbr = 0; request_nbr < 10; request_nbr++)
+	{
+		zmq_msg_t request;
+		zmq_msg_init_size (&request, 5);
+		memcpy (zmq_msg_data (&request), "Hello", 5);
+		printf ("Sending Hello %d...\n", request_nbr);
+		zmq_send (requester, &request, 0);
+		zmq_msg_close (&request);
+
+		zmq_msg_t reply;
+		zmq_msg_init (&reply);
+		zmq_recv (requester, &reply, 0);
+		//printf ("Received World %d\n", request_nbr);
+		snprintf (buf, 6, "%s", (char *)zmq_msg_data (&reply));
+		printf ("Receive %s %d\n", buf, request_nbr);
+
+		zmq_msg_close (&reply);
+	}
+	zmq_close (requester);
+	zmq_term (context);
+	return 0;
+}
---
+++ zeromq/tutor/wuserver.c
@@ -0,0 +1,33 @@
+//
+// Weather update server
+// Binds PUB socket to tcp://*:5556
+// Publishes random weather updates
+//
+#include "zhelpers.h"
+
+int main (void)
+{
+	// Prepare our context and publisher
+	void *context = zmq_init (1);
+	void *publisher = zmq_socket (context, ZMQ_PUB);
+	zmq_bind (publisher, "tcp://*:5556");
+	zmq_bind (publisher, "ipc://weather.ipc");
+
+	// Initialize random number generator
+	srandom ((unsigned) time (NULL));
+	while (1) {
+		// Get values that will fool the boss
+		int zipcode, temperature, relhumidity;
+		zipcode = randof (100000);
+		temperature = randof (215) - 80;
+		relhumidity = randof (50) + 10;
+
+		// Send message to all subscribers
+		char update [20];
+		sprintf (update, "%05d %d %d", zipcode, temperature, relhumidity);
+		s_send (publisher, update);
+	}
+	zmq_close (publisher);
+	zmq_term (context);
+	return 0;
+}
