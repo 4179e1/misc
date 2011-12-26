2a16ab207cd4f2808557a4b7548f5bacb4a132f0
lyre <lyre@poetpalace.org>
2011-12-26 23:46:45 +0800

    python: add introspection example

---
+++ python/introspection/callable.py
@@ -0,0 +1,6 @@
+import string
+print string.punctuation
+print string.join
+print callable (string.punctuation)
+print callable (string.join)
+print string.join.__doc__
---
+++ python/introspection/str.py
@@ -0,0 +1,6 @@
+print str(1)
+horsemen = ['war', 'pestilence', 'famine']
+print str(horsemen)
+import odbchelper
+print str(odbchelper)
+print str(None)
