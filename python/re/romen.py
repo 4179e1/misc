65e64914db5a0ac925a6d656755ad687e52055bb
lyre <lyre@poetpalace.org>
2012-01-06 21:23:03 +0800

    python: add verbos regular expersion example

--- python/re/romen.py
+++ python/re/romen.py
@@ -24,7 +24,7 @@ print re.search (pattern, 'MCMLXXXX')
 pattern= '^M?M?M?(CM|CD|D?C?C?C?)(XC|XL|L?X?X?X?)|(IX|IV|V?I?I?I?)$'
 
 pattern = '^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$'
-print re.search (pattern, 'MDLV')
+print re.search (pattern, 'MDLV').groups()
 print re.search (pattern, 'MMDCLXVI')
 print re.search (pattern, 'MMMDCCCLXXXVIII')
 print re.search (pattern, 'I')
