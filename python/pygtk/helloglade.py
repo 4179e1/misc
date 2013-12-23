d0479c42a0fc2869039be959a9b98b2f83d9d4a3
lyre <4179e1@gmail.com>
2013-12-23 17:28:42 +0800

    update helloglade (pygtk notebook)

--- python/pygtk/helloglade.py
+++ python/pygtk/helloglade.py
@@ -5,32 +5,46 @@ import gtk
 import gtk.glade
 
 class HelloGlade:
+    def open_clicked (self, widget, data=None):
+        self.fc.show()
+    def on_file_activated (self, widget, data=None):
+        self.filename = self.fc.get_filename()
+        self._set_status (self.filename.split('/')[-1])
+
+        self.fc.hide()
+        self.file = open (self.filename, 'rw')
+        textbuffer = self.tv.get_buffer()
+        textbuffer.set_text (self.file.read())
+
+
     def window_destroy (self, gobject, data=None):
         gtk.main_quit()
 
     def about_activate (self, widget, data=None):
-        print ('self', self)
-        print ('widget', widget)
-        print ('data', data)
         widget.show()
 
-    def about_delete_event (self, widget, event, data=None):
-        print ('delete_event signal', data)
-        print ('self', self)
-        print ('widget', widget)
-        print ('event', event)
-        print ('data', data)
+    def hide_on_delete_event (self, widget, event, data=None):
         widget.hide()
         return True
+    def _set_status (self, text):
+        self.sb.pop (self.sbid)
+        self.sb.push (self.sbid, text)
 
     def __init__ (self):
+        self.filename = None
         self.gladefile = 'helloglade.xml'
         self.builder = gtk.Builder()
         self.builder.add_from_file (self.gladefile)
         self.builder.connect_signals(self)
+
         self.window = self.builder.get_object ('window')
+        self.sb = self.builder.get_object ('statusbar')
+        self.sbid = self.sb.get_context_id ('untitiled')
+        self._set_status ('untitiled')
         self.window.show()
 
+        self.fc = self.builder.get_object ('filechooserdialog')
+        self.tv = self.builder.get_object ('textbuffer')
 
 if __name__ == '__main__':
     helloglade = HelloGlade()
