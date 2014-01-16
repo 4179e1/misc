466311bfab3e9caddcfd77b85bfc5651675ff66f
lyre <4179e1@gmail.com>
2014-01-16 23:27:06 +0800

    python: launcelot

---
+++ python/network/launcelot/launcelot.py
@@ -0,0 +1,28 @@
+import socket, sys
+
+PORT = 1234
+qadict = {
+'What is your name?': 'My name is Sir Launcelot of Camelot.',
+'What is your quest?': 'To seek the Holy Grial.',
+'What is your favorite color?': 'Blue.'}
+
+def recv_until (sock, suffix):
+    message = ''
+    while not message.endswith (suffix):
+        data = sock.recv(4096).strip('\n')
+        if not data:
+            raise EOFError ('socket closed before we saw %r' % suffix)
+        message += data
+    return message
+
+def setup():
+    interface = ''
+    if len (sys.argv) >= 2:
+        interface = sys.argv[1]
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind ((interface, PORT))
+    sock.listen (128)
+    print 'Ready and listening at %r port %d' % (interface, PORT)
+    return sock
+
---
+++ python/network/launcelot/simple.py
@@ -0,0 +1,19 @@
+import launcelot
+
+def handle_client (client_sock):
+    try:
+        while True:
+            question = launcelot.recv_until (client_sock, '?')
+            answer = launcelot.qadict.get (question, "I don't understand your question.")
+            client_sock.sendall (answer + '\n')
+    except EOFError:
+        client_sock.close()
+
+def server_loop (listen_sock):
+    while True:
+        client_sock, sockname = listen_sock.accept()
+        handle_client (client_sock)
+
+if __name__ == '__main__':
+    listen_sock = launcelot.setup()
+    server_loop (listen_sock)
