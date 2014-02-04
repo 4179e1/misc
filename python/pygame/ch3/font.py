88a7521327791c4993fb21da65eed22fcafd3209
lyre <4179e1@gmail.com>
2014-02-04 22:02:24 +0800

    pygame update

---
+++ python/pygame/ch3/font.py
@@ -0,0 +1,6 @@
+my_name = 'Lyre'
+import pygame
+pygame.init()
+my_font = pygame.font.SysFont("arial", 64)
+name_surface = my_font.render(my_name, True, (0, 0, 0), (255,255,255))
+pygame.image.save (name_surface, "name.png")
