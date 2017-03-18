fe9288f8e96e30c57ea46a74d300ab67202cc9d4
Leo Li <y.yong.li@oracle.com>
2017-03-18 13:44:23 +0800

    jinja

--- python/jinja2/run.py
+++ python/jinja2/run.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask (__name__)
 
+@app.template_filter()
+def datetimefilter (value, format='%Y/%m/%d %H:%M'):
+	"""convrert a datetime to a different format."""
+	return value.strftime(format)
+
+app.jinja_env.filters['datetimefilter'] = datetimefilter
+
 @app.route ("/")
 def template_test ():
-	return (render_template ('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="Home"))
+	return (render_template ('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="Index", current_time=datetime.datetime.now()))
+
+@app.route ("/home")
+def home ():
+	return (render_template ('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="Home", current_time=datetime.datetime.now()))
+
+@app.route ("/about")
+def about ():
+	return (render_template ('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="About", current_time=datetime.datetime.now()))
+
+@app.route ("/contact")
+def contact ():
+	return (render_template ('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="Contact", current_time=datetime.datetime.now()))
 
 if __name__ == '__main__':
 	app.run (debug=True)
