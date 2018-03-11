46d27ebe41a9444b31eb4792b82eccf9adfa87e6
lyre <lyre@poetpalace.org>
2018-03-11 21:04:46 +0800

    go by example

---
+++ go/src/bigdigits/bigdigits.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "path/filepath"
+)
+
+var bigDigits = [][]string{
+    {"  000  ",
+     " 0   0 ",
+     "0     0",
+     "0     0",
+     "0     0",
+     " 0   0 ",
+     "  000  "},
+    {" 1 ", "11 ", " 1 ", " 1 ", " 1 ", " 1 ", "111"},
+    {" 222 ", "2   2", "   2 ", "  2  ", " 2   ", "2    ", "22222"},
+    {" 333 ", "3   3", "    3", "  33 ", "    3", "3   3", " 333 "},
+    {"   4  ", "  44  ", " 4 4  ", "4  4  ", "444444", "   4  ",
+        "   4  "},
+    {"55555", "5    ", "5    ", " 555 ", "    5", "5   5", " 555 "},
+    {" 666 ", "6    ", "6    ", "6666 ", "6   6", "6   6", " 666 "},
+    {"77777", "    7", "   7 ", "  7  ", " 7   ", "7    ", "7    "},
+    {" 888 ", "8   8", "8   8", " 888 ", "8   8", "8   8", " 888 "},
+    {" 9999", "9   9", "9   9", " 9999", "    9", "    9", "    9"},
+}
+
+func main () {
+    if len (os.Args) <= 1 {
+        fmt.Println ("usage: %s <whole-number>\n", filepath.Base (os.Args[0]))
+        os.Exit (1)
+    }
+
+    stringofDigits := os.Args[1]
+
+    for row := range bigDigits[0] {
+        line := ""
+
+        for column := range stringofDigits {
+            digit := stringofDigits[column] - '0'
+            if 0 <= digit && digit <= 9 {
+                line += bigDigits[digit][row] + "  "
+            } else {
+                log.Fatal ("invalid whole-number")
+            }
+        }
+
+        fmt.Println (line)
+    }
+
+}
--- python/flask/quickstart/app.fcgi
+++ python/flask/quickstart/app.fcgi
@@ -1,6 +1,56 @@
-#!/usr/bin/python
-from flup.server.fcgi import WSGIServer
-from sessions import app
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
+from cgi import escape
+import sys, os
+from flup.server.fcgi import WSGIServer
+from flask import Flask
+import GoogleParser
+
+app = Flask (__name__)
+
+@app.route ('/')
+def index():
+	return "Hello world!"
+
+@app.route ('/search')
+def search ():
+	e500=""" <!DOCTYP html>
+<html>
+<head>
+<title>500 Internal Server Error</title>
+</head>
+<body>
+<h1>500 Internal Server Error</h1>
+</body>
+</html>
+"""
+
+	qs = environ['QUERY_STRING']
+
+	return qs
+#	try:
+#		gp = GoogleParser.GoogleParser (qs)
+#	except KeyError:
+##		start_response('302 Found', [('Location', 'http://poetpalace.org/search.html')])
+#		yield redirect ('http://poetpalace.org/search.html')
+#	except Exception:
+##		start_response('200 OK', [('Content-Type', 'text/html')])
+#		import syslog
+#		e = sys.exc_info()[0]
+#		syslog.openlog ("search.py")
+#		syslog.syslog(syslog.LOG_ERR, str(e))
+#		syslog.closelog()
+#		yiedl (e500)
+#	except Excption as e:
+##		start_response('200 OK', [('Content-Type', 'text/html')])
+#		yield (repr (e))
+#		
+#
+##	start_response('200 OK', [('Content-Type', 'text/html')])
+#	for i in gp.iter():
+#		yield (i)
+	
 if __name__ == '__main__':
-	WSGIServer (app).run()
+	WSGIServer(app).run()
+
