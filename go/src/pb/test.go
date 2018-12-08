d1bc7c2ceaad1764e1a44c744f8dd056a32ba042
lyre <lyre@poetpalace.org>
2018-12-08 20:21:16 +0800

    pb marshal/unmarshal

--- go/src/pb/test.go
+++ go/src/pb/test.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"reflect"
 	pb	"pb/tutorial"
+	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 )
 
 func main() {
-	p := pb.Person {
+	p := &pb.Person {
 		Id: 1234,
 		Name: "jdoe@example.com",
 		Phones: []*pb.Person_PhoneNumber {
@@ -17,6 +20,25 @@ func main() {
 
 	p.LastUpdated = ptypes.TimestampNow()
 
-	fmt.Println (p)
+	fmt.Printf ("%V\n", p)
 	fmt.Println (p.GetLastUpdated())
+
+	out, err := proto.Marshal (p)
+	if err != nil {
+		log.Fatalln ("Failed to encode: ", err)
+	}
+
+	fmt.Println (out)
+
+	p2 := &pb.Person{}
+	if err := proto.Unmarshal (out, p2); err != nil {
+		log.Fatalln ("Failed to parse: ", err)
+	}
+
+	fmt.Printf ("%V\n", p2)
+
+	// I don't know why they were not equal...
+	if ! reflect.DeepEqual (p, p2) {
+		log.Fatalln ("Person mismatch")
+	}
 }
